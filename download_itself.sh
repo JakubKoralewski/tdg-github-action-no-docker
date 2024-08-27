6328def0559672c7147809e2e94162f52c6776f4
Jakub Koralewski <contact@jcubed.me>
2024-08-27 16:27:12 +0200

    Run without Docker

---
+++ download_itself.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Arguments
+ACTION_REPOSITORY=$1
+FILE=$2
+TOKEN=$3
+
+# Download the asset
+curl -L -H "Authorization: token $TOKEN" \
+  -H "Accept: application/octet-stream" \
+  "https://api.github.com/$ACTION_REPOSITORY/releases/latest/download/$FILE" \
+  -o "$FILE"
